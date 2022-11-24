41ec6c4f9233322cb1c4fbd548232886974833da
Riyyi <riyyi3@gmail.com>
2022-11-24 16:25:16 +0100

    Add scheme and controller for car manufacturer

---
+++ CarRegistration/CarManufacturer.cs
@@ -0,0 +1,14 @@
+namespace CarRegistration;
+
+public class CarManufacturer
+{
+    public string? Name { get; set; }
+
+    public string? Brand { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public int FoundingYear { get; set; }
+}
---
+++ CarRegistration/Controllers/CarManufacturerController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRegistration.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CarManufacturerController : ControllerBase
+{
+    private static readonly string[] Brands = new[]
+    {
+        "VW", "Audi", "Skoda", "Mercedes-Benz"
+    };
+
+    private readonly ILogger<CarManufacturerController> _logger;
+
+    public CarManufacturerController(ILogger<CarManufacturerController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCarManufacturer")]
+    public IEnumerable<CarManufacturer> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new CarManufacturer
+        {
+            Name = "",
+            Brand = Brands[Random.Shared.Next(Brands.Length)],
+            Address = "",
+            PhoneNumber = "",
+            FoundingYear = 1970
+        })
+        .ToArray();
+    }
+}
