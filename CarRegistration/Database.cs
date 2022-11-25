361522b76b45fe38dbffe734f0dd9b385f15c86a
Riyyi <riyyi3@gmail.com>
2022-11-25 16:50:51 +0100

    Add initial data to the database via seed

--- CarRegistration/Database.cs
+++ CarRegistration/Database.cs
@@ -21,4 +21,59 @@ public class CarRegistrationContext : DbContext
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CarManufacturer>().HasData(
+            new CarManufacturer {
+                Id = 1,
+                Name = "Big manufacturer",
+                Address = "Chinastreet 1",
+                PhoneNumber = "+86-13910998888",
+                FoundingYear = 1973
+            }
+        );
+
+        modelBuilder.Entity<Brand>().HasData(
+            new Brand {
+                Id = 1,
+                Name = "VW",
+                CarManufacturerId = 1,
+            },
+            new Brand {
+                Id = 2,
+                Name = "Audi",
+                CarManufacturerId = 1,
+            },
+            new Brand {
+                Id = 3,
+                Name = "Skoda",
+                CarManufacturerId = 1,
+            },
+            new Brand {
+                Id = 4,
+                Name = "Mercedes-Benz",
+                CarManufacturerId = 1,
+            },
+            new Brand {
+                Id = 5,
+                Name = "Toyota",
+                CarManufacturerId = 1,
+            }
+        );
+
+        modelBuilder.Entity<Car>().HasData(
+            new Car {
+                Id = 1,
+                MakeId = 5,
+                Model = "Aygo",
+                VIN = "4Y1SL65848Z411439",
+                LicensePlate = "3-DGP-63",
+                InitialRegistrationDate = new DateTime(2011,02,15),
+                Power = 53,
+                BodyType = "4-doors",
+                Color = "Blue",
+            }
+        );
+    }
 }
---
+++ CarRegistration/Migrations/20221125154808_AddSeedData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRegistration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CarManufacturers",
+                columns: new[] { "Id", "Address", "FoundingYear", "Name", "PhoneNumber" },
+                values: new object[] { 1L, "Chinastreet 1", 1973, "Big manufacturer", "+86-13910998888" });
+
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "CarManufacturerId", "Name" },
+                values: new object[,]
+                {
+                    { 1L, 1L, "VW" },
+                    { 2L, 1L, "Audi" },
+                    { 3L, 1L, "Skoda" },
+                    { 4L, 1L, "Mercedes-Benz" },
+                    { 5L, 1L, "Toyota" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "BodyType", "Color", "InitialRegistrationDate", "LicensePlate", "MakeId", "Model", "Power", "VIN" },
+                values: new object[] { 1L, "4-doors", "Blue", new DateTime(2011, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "3-DGP-63", 5L, "Aygo", 53, "4Y1SL65848Z411439" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "CarManufacturers",
+                keyColumn: "Id",
+                keyValue: 1L);
+        }
+    }
+}
